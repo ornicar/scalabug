85b8d491f7718331b29429944ae6728cfb7a6aec
Thibault Duplessis <t@lichess.org>
2022-11-12 17:30:33 +0100

    document example

--- extension-ambiguous/code.scala
+++ extension-ambiguous/code.scala
@@ -7,4 +7,21 @@ object B:
 import A.*
 import B.*
 
-def f = Option(1).foo
+val _ = Option(1).foo // error: value foo is not a member of Option[Int].
+
+/*
+[info] compiling 1 Scala source to /home/thib/scalabug/extension-ambiguous/target/scala-3.2.1/classes ...
+[error] -- [E008] Not Found Error: /home/thib/scalabug/extension-ambiguous/code.scala:10:18
+[error] 10 |def f = Option(1).foo
+[error]    |        ^^^^^^^^^^^^^
+[error]    |      value foo is not a member of Option[Int].
+[error]    |      An extension method was tried, but could not be fully constructed:
+[error]    |
+[error]    |          B.foo[T](Option.apply[Int](1))    failed with
+[error]    |
+[error]    |              Reference to foo is ambiguous,
+[error]    |              it is both imported by import A._
+[error]    |              and imported subsequently by import B._
+[error] one error found
+[error] (Compile / compileIncremental) Compilation failed
+ */
